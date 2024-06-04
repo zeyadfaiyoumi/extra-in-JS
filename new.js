ef4b6b273bd9be61583b39b31d8dab698767a96e
zeyadfaiyoumi <zeyadfaiyoumi@gmail.com>
2024-06-04 16:48:23 +0300

    extra-in-JS

---
+++ new.js
@@ -0,0 +1,32 @@
+for(i=0;i<=5;i++)
+    {alert(i);}
+    
+    var index=[] 
+    for(i=1;i<=5;i++)
+        {index.push(i)}
+    
+        alert(index);
+        var arr=[]
+        for(i=0;i<=20;i++)
+          {
+            if(i%3==0){arr.push(i);}
+    
+          } 
+          alert(arr) ;
+    
+          let randomNum= parseInt(prompt("Please enter randomNum"))
+          while(randomNum>100){
+            randomNum= parseInt(prompt("Please enter randomNum"))
+          }
+          while(randomNum<0){
+            randomNum= parseInt(prompt("Please enter randomNum"))
+          }
+        
+          var number= parseInt(prompt("Please enter Number"))
+          let sum=0;
+          for(i=number;i!=0;i--){
+           
+            sum+=i
+          }
+            alert (sum);
+    
